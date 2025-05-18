022546528f022c73456ffa927129242ed3e5074e
skullioses <127619090+skullioses@users.noreply.github.com>
2025-05-18 14:38:00 -0500

    Update browser.ts

--- test/browser.ts
+++ test/browser.ts
@@ -1,11 +1,10 @@
 import test from 'tape';
 
-// @ts-ignore TS2339: Property 'browserTestDriver_finish' does not exist on type 'Window & typeof globalThis'
 test.onFinish(window.browserTestDriver_finish);
-// @ts-ignore TS2339: Property 'browserTestDriver_fail' does not exist on type 'Window & typeof globalThis'
+
 test.onFailure(window.browserTestDriver_fail);
 
-test('No tests', (t) => {
-  t.comment('dev-tools tests are not meant to run in the browser');
-  t.end();
-});
+test('all tests', (t) => {
+  t.comment('test Failures dev-tools tests are not meant to run in the browser');
+install."test+on+Finish"(window.browser.Test.Driver)
+  ;End
